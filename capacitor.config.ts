328eec9e07abd094d75215522d41344a0da8150b
Matheus Rafalski <matheus__rafalski@hotmail.com>
2023-11-01 19:21:34 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.myApp.app',
   appName: 'myApp',
   webDir: 'dist',
   server: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,90 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom"
 import {
   IonApp,
+  IonButton,
+  IonGrid,
   IonIcon,
+  IonImg,
+  IonItem,
   IonLabel,
+  IonLoading,
   IonRouterOutlet,
+  IonRow,
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-
+  IonText,
+  IonTitle,
+  setupIonicReact,
+} from "@ionic/react"
+import { IonReactRouter } from "@ionic/react-router"
+import { ellipse, square, triangle } from "ionicons/icons"
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css"
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css"
+import "@ionic/react/css/structure.css"
+import "@ionic/react/css/typography.css"
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css"
+import "@ionic/react/css/float-elements.css"
+import "@ionic/react/css/text-alignment.css"
+import "@ionic/react/css/text-transformation.css"
+import "@ionic/react/css/flex-utils.css"
+import "@ionic/react/css/display.css"
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css"
+import "./app.css"
+import { useCallback, useState } from "react"
 
-setupIonicReact();
+setupIonicReact()
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [image, setImage] = useState("https://pic.re/image")
+  const [loading, setLoading] = useState(false)
 
-export default App;
+  function getImage() {
+    console.log("al")
+
+    setLoading(true)
+    const timestamp = new Date().getTime()
+    const imageUrl = `https://pic.re/image/${timestamp}`
+    setImage(imageUrl)
+    setLoading(false)
+  }
+
+  return (
+      <IonGrid>
+        <IonRow>
+          <IonTitle className="ion-no-padding">Bem-vindo ao Meu Site</IonTitle>
+        </IonRow>
+        <IonRow className="ion-margin-top">
+          <IonText>Este é um parágrafo exemplo</IonText>
+        </IonRow>
+        <IonRow>
+          <IonButton onClick={() => alert("olá")}>Clique aqui</IonButton>
+        </IonRow>
+        <IonTitle className="ion-no-padding">Seção importante</IonTitle>
+        <IonRow className="ion-margin-top">
+          <IonText>Esta é uma seção com conteúdo relevante</IonText>
+        </IonRow>
+        <IonImg
+          className="sm"
+          style={{ maxWidth: 300 }}
+          src="https://www.unijui.edu.br/images/stories/manual_identidade/marcas/download.php?arquivo=marca.jpg"
+        />
+        <IonRow>
+          <IonButton id="trigger" onClick={getImage}>
+            Gerar Imagem
+          </IonButton>
+        </IonRow>
+        <IonImg style={{"width": 400}} src={image} />
+        <IonLoading trigger="trigger" duration={2000} />
+      </IonGrid>
+  )
+}
+
+export default App
